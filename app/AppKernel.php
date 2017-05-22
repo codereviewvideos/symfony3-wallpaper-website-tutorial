c636f70add080a2ccbeb330f09f29d3e9b01a915
Chris <chris@codereviewvideos.com>
2017-05-22 10:57:05 +0100

    pagination

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+
             new AppBundle\AppBundle(),
         ];
 
--- src/AppBundle/Controller/GalleryController.php
+++ src/AppBundle/Controller/GalleryController.php
@@ -10,24 +10,30 @@ class GalleryController extends Controller
 {
     /**
      * @Route("/gallery", name="gallery")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $images = [
+            'landscape-summer-beach.jpg',
+            'landscape-summer-field.jpg',
+            'landscape-summer-flowers.jpg',
+            'landscape-summer-hill.jpg',
+            'landscape-summer-mountain.png',
+            'landscape-summer-sea.jpg',
+            'landscape-summer-sky.jpg',
+        ];
+
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $images,
+            $request->query->getInt('different', 1)/*page number*/,
+            4/*limit per page*/
+        );
+
         return $this->render('gallery/index.html.twig', [
-            'images' => [
-                'summer-morning-in-fort-collins.jpg',
-                'summer-morning-in-fort-collins.jpg',
-                'summer-morning-in-fort-collins.jpg',
-                'summer-morning-in-fort-collins.jpg',
-                'summer-morning-in-fort-collins.jpg',
-                'summer-morning-in-fort-collins.jpg',
-                'summer-morning-in-fort-collins.jpg',
-                'summer-morning-in-fort-collins.jpg',
-                'summer-morning-in-fort-collins.jpg',
-                'summer-morning-in-fort-collins.jpg',
-                'summer-morning-in-fort-collins.jpg',
-                'summer-morning-in-fort-collins.jpg',
-            ]
+            'images' => $pagination,
         ]);
     }
 }
